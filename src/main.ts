14e95a3c6b49d14fcc853003a2600228d1a66b6e
Dmitry Baev <baev.dm@gmail.com>
2021-04-01 18:21:16 +0300

    add action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,61 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+interface Label {
+  color: string;
+  default: boolean;
+  description: string;
+  name: string;
+  node_id: string;
+  url: string;
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const allowedLabels: string[] = core
+      .getInput("allowed", { required: true })
+      .split("\n")
+      .filter((x) => x !== "");
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`allowed labels: ${allowedLabels}`);
 
-    core.setOutput('time', new Date().toTimeString())
+    const pr = github.context.payload.pull_request;
+    if (!pr) {
+      core.info("not a pull request");
+      return;
+    }
+
+    const prLabels: Label[] | undefined = pr?.labels;
+    const labels = prLabels?.map((l) => l.name);
+
+    core.debug(`pull request labels: ${labels}`);
+
+    if (!labels?.length) {
+      core.setFailed("pull request has no labels");
+      return;
+    }
+
+    const matched: string[] = [];
+    const notMatched: string[] = [];
+    for (const l of labels) {
+      if (allowedLabels.find((al) => al.toLowerCase() === l.toLowerCase())) {
+        matched.push(l);
+      } else {
+        notMatched.push(l);
+      }
+    }
+
+    core.debug(`matched ${matched}`);
+    core.debug(`not matched ${notMatched}`);
+
+    if (matched.length !== 1) {
+      core.setFailed(`expecting only one matched label, but found ${matched}`);
+    } else {
+      core.info(`found exactly one allowed label ${matched[0]}`);
+    }
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
